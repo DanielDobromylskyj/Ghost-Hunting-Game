311c042578b44c56f3829256fd822103697331f3
DanielDobromylskyj <90842024+DanielDobromylskyj@users.noreply.github.com>
2025-09-18 23:15:42 +0100

    Usefull stuff

--- main.py
+++ main.py
@@ -1,5 +1,16 @@
 from engine import game
 
+"""
+How to host on uni wifi
+
+Open cmd -> run "ssh -R 80:localhost:5678 serveo.net"
+Copy the Hostname from the terminal (Do not include final slash or starting https://)
+Use that hostname for external clients
+
+"""
+
+
+debug = input("Debug Mode? (Y/n)").lower() != "n"
 
 instance = game.Game()
 instance.start()
